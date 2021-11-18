d8e502fe34e2ad229266f1a7738078247d5dc760
Marcelo Figueira <marcelosousafigueira@gmail.com>
2021-11-18 13:07:47 -0300

    front end arrumado

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
-import { Router } from 'express';
-const routes = new Router();
-import UserController from './app/controllers/UserController'
-import SessionControler from './app/controllers/SessionController'
+import express from 'express';
+const routes = express();
+import UserController from './app/controllers/UserController';
+import SessionControler from './app/controllers/SessionController';
+import Tratativa from './tratativa';
+
+routes.use(express.static(__dirname + '/pages/assets/js/'));
+routes.use(express.static(__dirname + '/pages/assets/css/'));
+routes.use(express.static(__dirname + '/pages/assets/img/'))
+//routes.use(express.static('pages'));
+routes.set('view engine', 'ejs');
+
 
 routes.get('/', (req, res) => {
-    res.sendFile(__dirname + "/pages/index.html");
-})
+    res.render(__dirname + "/pages/home");
+});
+
+routes.post('/tratativa', Tratativa.erro);
 
 routes.post('/user', UserController.store);
 
---
+++ src/tratativa.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express()
+
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+
+
+export default new Tratativa();
+
+
